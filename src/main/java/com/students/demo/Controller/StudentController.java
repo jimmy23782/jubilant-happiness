0c01841c655c592815c6dedac03af863e1dc00e4
Jimmy Joy <jimmyjoy23782@gmail.com>
2025-09-05 09:01:39 +1000

    Add basic Logging

--- src/main/java/com/students/demo/Controller/StudentController.java
+++ src/main/java/com/students/demo/Controller/StudentController.java
@@ -6,10 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.students.demo.Service.StudentControllerService;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1")
+@Slf4j
 public class StudentController {
 
     private final StudentControllerService service;
@@ -21,6 +24,7 @@ public class StudentController {
     @GetMapping("/students")
     public List<String> getStudents() {
         // Example payload; replace with a service call later
+        log.info("Received request for student list");
         return service.studentList();
     }
 }
--- src/main/java/com/students/demo/Service/StudentControllerService.java
+++ src/main/java/com/students/demo/Service/StudentControllerService.java
@@ -2,13 +2,16 @@ package com.students.demo.Service;
 
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class StudentControllerService {
     
     public List<String> studentList(){
 
+        log.info("Fetching student list");
         return List.of("Alice", "Bob", "Charlie","Lodada");
     }
 }
